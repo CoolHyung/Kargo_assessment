07497b9a6429d8e908bb432a55d0d3acca6f59b6
CoolHyung <hyungjoolee131@gmail.com>
2021-04-28 03:07:01 +0100

    created an edge case for just zero and answer output format

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var rand = [5, 32, 123];
+var rand = [5, 32, 123, 0];
 
-function express(arr){
+function convert(arr){
 
     var ans = []; //this is the answer that will be returned later
     var wordform; //the word that has been translated to from the number in the list
@@ -8,11 +8,15 @@ function express(arr){
     for (var i = 0; i < arr.length; i++) {
         var n = arr[i];
         wordform = '';
+        if (n==0){
+            wordform = change(n);
+
+        }
         while (n!=0) {
             
             var val = n % 10;
             n = (n- (n%10)) / 10;
-            console.log(n);
+            //console.log(n);
             wordform = change(val) + wordform;
         }
 
@@ -26,5 +30,5 @@ function change(num){
     var numbers = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'];
     return numbers[num];
 }
-
-console.log(express(rand));
+var answer = convert(rand);
+console.log(answer.join());
